33731c0ab5c8886c8aeff35c0102724588e814b7
Randell Anderson <zugzug07@gmail.com>
2021-06-26 10:46:51 -0500

    Week 2 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -73,6 +73,7 @@ campsiteRouter.route('/:campsiteId')
 	.catch(err => next(err));
 });
 
+//comments
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
 	Campsite.findById(req.params.campsiteId)
